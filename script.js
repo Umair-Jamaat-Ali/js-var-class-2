3222ec9eefe2ca0bd1bb4995a5fef164f05a571e
Umair-Jamaat-Ali <umairansari758@gmail.com>
2023-03-19 12:32:08 +0500

    AND opration

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // true or false statements
+// Greather than , less than , equal to and not-equal to
 
 var result = 5<4
 console.log("Result =>", result)
@@ -9,3 +10,16 @@ console.log("Result1 =>", result1)
 
 var result2 = 5>=4
 console.log("Result2 =>", result2)
+
+var result3 = 5==5
+console.log("Result3 =>", result3)
+
+var result4 = 6 != 6
+console.log("Result4", result4)
+
+// AND operation, in java script is written as ( &&)
+// T T = T
+// T F = F
+// F T = F
+// F F = F
+// LET CHECK BY PRACTICE
