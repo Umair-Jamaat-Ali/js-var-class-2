2c5dd8d89e040fefdae036ddb9110b13c99d401c
Umair-Jamaat-Ali <umairansari758@gmail.com>
2023-03-19 11:54:45 +0500

    var class 2

---
+++ script.js
@@ -0,0 +1,11 @@
+// true or false statements
+
+var result = 5<4
+console.log("Result =>", result)
+
+
+var result1 = 6>7
+console.log("Result1 =>", result1)
+
+var result2 = 5>=4
+console.log("Result2 =>", result2)
